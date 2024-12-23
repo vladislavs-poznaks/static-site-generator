7f24cd066ab67c832f54c657c8c588b1449ac974
vladislavs-poznaks <vladislavs.poznaks@viasmsgroup.com>
2024-12-23 11:34:59 +0200

    added recursive static to public copy

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+from contents import *
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    node = TextNode("test", TextType.NORMAL, "www.example.com")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    print(node)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 if __name__ == "__main__":
     main()
